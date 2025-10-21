e87ae61ecdaf60b054aa63fe821d00106a94d5ee
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-10-21 11:11:19 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int Recursive(int kyuuryou, int zikan, int kotei) {
     // 現在の給料と固定給を表示
     printf("時給: %d円　固定給: %d円\n", kyuuryou, kotei);
 
-    // 次の時間の給料を計算（前の時給×2−50）
+    // 次の時間の給料を計算
     kyuuryou = kyuuryou * 2 - 50;
 
     // 再帰呼び出し（時間を1増やす）
@@ -22,7 +22,7 @@ int main() {
 
     int saiki = 100;   // 最初の時給
     int ippan = 1226;  // 一般的な時給
-    int zikan = 1;     // 1時間目スタート（0でもOK）
+    int zikan = 1;     // 1時間目スタート
 
     int result = Recursive(saiki, zikan, ippan);
 
